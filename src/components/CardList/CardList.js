8f2e6b7f6e5c218930f34918af175ac820d6508a
Brian <briangordon.cahill@students.dominican.edu>
2023-05-02 14:54:55 -0700

    Website is now responsive mobile and desktop

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -5,8 +5,8 @@ import './CardList.css';
 export default function CardList({ data, menuSelection, searchText }) {
   return (
     <div className="card-list">
-      {!data ? 
-      <p> Loading...</p> : 
+      {!data || !data.data ? 
+      <p style={{color: "white", margin: "auto", fontSize: "40px"}}> Loading...</p> : 
         data.data.filter(obj => obj.name.toLowerCase().includes(searchText.toLowerCase())).map((card) => (
           <Card data={card} key={card.id} menuSelection={menuSelection}/>
         ))}
